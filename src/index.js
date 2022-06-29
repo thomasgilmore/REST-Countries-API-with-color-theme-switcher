22bd861d2a5f9daacfd733d860eb7048122b6d06
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-06-29 16:57:17 -0400

    CountriesContextProvider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CountriesContextProvider } from './Context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CountriesContextProvider>
+      <App />
+    </CountriesContextProvider>
   </React.StrictMode>
 );
 
