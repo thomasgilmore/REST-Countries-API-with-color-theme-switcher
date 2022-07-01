ba7f439f13315a3ec39d0d3f552721a4d046306b
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-06-30 21:32:29 -0400

    import Filter.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react';
 import CountryCard from './CountryCard';
 import { CountriesContext } from '../Context/Context';
+import Filter from './Filter';
 
 export default function Home() {
   const { countryList } = useContext(CountriesContext);
-  console.log("home", countryList);
+  
   return (
     <div>
+      <Filter />
       <div className='flex flex-wrap justify-center gap-12'>
         {countryList.length > 0 ? countryList.map((country) => {
           return (<CountryCard key={country.name} img={country.flags.png} name={country.name} population={country.population} region={country.region} capital={country.capital} />)
