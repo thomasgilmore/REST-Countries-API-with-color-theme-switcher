25681b4192ad1f1efb70c73881d1d61c4abd732f
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-06-30 20:41:27 -0400

    working on Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import CountryCard from './CountryCard';
+import { CountriesContext } from '../Context/Context';
 
 export default function Home() {
+  const { countryList } = useContext(CountriesContext);
+  console.log("home", countryList);
   return (
-    <div>Home</div>
+    <div>
+      <div className='flex flex-wrap justify-center gap-12'>
+        {countryList.length > 0 ? countryList.map((country) => {
+          return (<CountryCard key={country.name} img={country.flags.png} name={country.name} population={country.population} region={country.region} capital={country.capital} />)
+        }) : null }
+      </div>
+    </div>
   )
 }
