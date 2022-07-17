fb408df7ed434646ce23cad5e601954d9458f150
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-16 21:38:43 -0400

    working on Title.js

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -7,11 +7,11 @@ import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 export default function Title() {
-  const { darkMode, handleDarkMode, darkTheme } = useContext(CountriesContext);
+  const { darkMode, handleDarkMode } = useContext(CountriesContext);
   return (
     <div className='flex m-5 justify-between'>
       <Typography variant='h4'>Where in the world?</Typography>
-      <Button className='darkModeButton' onClick={handleDarkMode} variant="text">{darkMode ? <DarkModeIcon /> : <DarkModeOutlinedIcon />}Dark Mode</Button>
+      <Button className='darkModeButton' onClick={handleDarkMode} variant="text">{darkMode ? <DarkModeIcon className='mr-2' /> : <DarkModeOutlinedIcon className='mr-2' />}Dark Mode</Button>
     </div>
   )
 }
