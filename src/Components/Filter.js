53812b1754318edf63190e3735e236c4b1cb8f22
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-17 16:08:41 -0400

    background color for Filter.js inputs

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -7,9 +7,10 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { Input } from '@mui/material';
 
 export default function Filter() {
-  const { handleInputChange, handleSearchSubmit } = useContext(CountriesContext);
+  const { handleInputChange, handleSearchSubmit, region, handleRegionChange, handleSelectRegionSearch, darkMode } = useContext(CountriesContext);
 
   const StyledInputElement = styled('input')(
     ({ theme }) => `
@@ -23,20 +24,21 @@ export default function Filter() {
   return (
     <div className='flex m-5 justify-between'>
     <div className='flex items-center rounded-lg p-1 border-2'>
-      <SearchIcon />
-      <InputUnstyled className='m-1' onChange={handleSearchSubmit} placeholder='Search for a country...'/>
+      <SearchIcon style={{ color : darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(0, 0%, 52%)' }} />
+      <Input className='m-1' onChange={handleSearchSubmit} style={{ backgroundColor : darkMode ? 'hsl(207, 26%, 17%)' : 'hsl(0, 0%, 100%)' }} placeholder='Search for a country...'/>
     </div>
-      <FormControl sx={{ width: 200 }}>
+      <FormControl sx={{ width: 200, backgroundColor : darkMode ? 'hsl(207, 26%, 17%)' : 'hsl(0, 0%, 100%)' }}>
         <InputLabel id="demo-simple-select-label">Filter by Region</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value=""
+          value={region}
           label="Region"
-          // onChange={handleChange}
+          onChange={handleSelectRegionSearch}
         >
+          <MenuItem value=""><em>None</em></MenuItem>
           <MenuItem value="Africa">Africa</MenuItem>
-          <MenuItem value="America">America</MenuItem>
+          <MenuItem value="Americas">Americas</MenuItem>
           <MenuItem value="Asia">Asia</MenuItem>
           <MenuItem value="Europe">Europe</MenuItem>
           <MenuItem value="Oceania">Oceania</MenuItem>
