fcabdd0821dd58e7107fbe0dfe534c4fc942d9b7
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-18 15:42:28 -0400

    added Title and Country Route to App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React, { useContext } from 'react';
 import { CountriesContext } from './Context/Context';
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
+import Country from "./Components/Country";
+import Title from "./Components/Title";
 
 function App() {
   const { theme, darkMode } = useContext(CountriesContext);
   return (
     <ThemeProvider theme={theme}>
       <div style={{ backgroundColor: darkMode ? 'hsl(207, 26%, 17%)' : 'hsl(0, 0%, 98%)' }}>
+        <Title />
         <Routes>
           <Route path="*" element={<Home />} />
+          <Route path="/country" element={<Country />} />
         </Routes>
       </div>
     </ThemeProvider>
