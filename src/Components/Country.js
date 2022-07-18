8e36ca14bbaa2b38418271145622293044dc18d9
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-18 15:42:44 -0400

    Country.js setup

---
+++ src/Components/Country.js
@@ -0,0 +1,9 @@
+import React, { useContext } from 'react';
+import { CountriesContext } from '../Context/Context';
+
+export default function Country() {
+  const {  } = useContext(CountriesContext);
+  return (
+    <div>Country</div>
+  )
+}
