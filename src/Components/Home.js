d628e8684f706855f31f059acc7f4adaaf68a71d
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-18 15:43:26 -0400

    removed Title.js from Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,16 +2,13 @@ import React, { useContext } from 'react';
 import CountryCard from './CountryCard';
 import { CountriesContext } from '../Context/Context';
 import Filter from './Filter';
-import Title from './Title';
 
 export default function Home() {
   const { countryList } = useContext(CountriesContext);
-  
   return (
     <div>
-      <Title />
       <Filter />
-      <div className='flex flex-wrap justify-center gap-12'>
+      <div className='flex flex-wrap justify-center gap-20'>
         {countryList.length > 0 ? countryList.map((country) => {
           return (<CountryCard key={country.name} img={country.flags.png} name={country.name} population={country.population} region={country.region} capital={country.capital} />)
         }) : null }
