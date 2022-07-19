f78ea64c112be269b87a1748d390c267b5bc8267
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-18 20:59:05 -0400

    added style to App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const { theme, darkMode } = useContext(CountriesContext);
   return (
     <ThemeProvider theme={theme}>
-      <div style={{ backgroundColor: darkMode ? 'hsl(207, 26%, 17%)' : 'hsl(0, 0%, 98%)' }}>
+      <div style={{ backgroundColor: darkMode ? 'hsl(207, 26%, 17%)' : 'hsl(0, 0%, 98%)', minHeight: '100vh' }}>
         <Title />
         <Routes>
           <Route path="*" element={<Home />} />
