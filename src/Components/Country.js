0c4a9178694ea10cef6a206aa676f69ab3637654
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-07-18 21:36:15 -0400

    changed color of text on dark mode

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -14,19 +14,19 @@ export default function Country() {
       <div className='flex flex-wrap space-x-10 m-10 justify-center'>
         <img src={selectedCountry.flag} style={{ width: '35%', minWidth: 300, maxWidth: 750, marginRight: 30, marginBottom: 20 }} alt={selectedCountry.flag} />
         <div className='m-auto'>
-          <Typography style={{ fontWeight: 'bold', marginBottom: 10 }} variant='h3'>{selectedCountry.name}</Typography>
+          <Typography style={{ fontWeight: 'bold', marginBottom: 10 }} color={darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)'} variant='h3'>{selectedCountry.name}</Typography>
           <div className='flex'>
             <div className='flex flex-col mr-10 gap-2'>
-              <Typography variant="body" color="text"><b>Nativ Name:</b> {selectedCountry.nativeName}</Typography>
-              <Typography variant="body" color="text"><b>Population:</b> {selectedCountry.population}</Typography>
-              <Typography variant="body" color="text"><b>Region:</b> {selectedCountry.region}</Typography>
-              <Typography variant="body" color="text"><b>Sub Region:</b> {selectedCountry.subregion}</Typography>
-              <Typography variant="body" color="text"><b>Capital:</b> {selectedCountry.capital}</Typography>
+              <Typography variant="body" color={darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)'}><b>Nativ Name:</b> {selectedCountry.nativeName}</Typography>
+              <Typography variant="body" color={darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)'}><b>Population:</b> {selectedCountry.population}</Typography>
+              <Typography variant="body" color={darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)'}><b>Region:</b> {selectedCountry.region}</Typography>
+              <Typography variant="body" color={darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)'}><b>Sub Region:</b> {selectedCountry.subregion}</Typography>
+              <Typography variant="body" color={darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)'}><b>Capital:</b> {selectedCountry.capital}</Typography>
             </div>
             <div className='flex flex-col gap-2'>
-              <Typography variant="body" color="text"><b>Top level Domain:</b> {selectedCountry.topLevelDomain[0]}</Typography>
-              <Typography variant="body" color="text"><b>Currencies:</b> {selectedCountry.currencies[0].name}</Typography>
-              <Typography variant="body" color="text"><b>Lanuages:</b> {selectedCountry.languages[0].name}</Typography>
+              <Typography variant="body" color={darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)'}><b>Top level Domain:</b> {selectedCountry.topLevelDomain[0]}</Typography>
+              <Typography variant="body" color={darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)'}><b>Currencies:</b> {selectedCountry.currencies[0].name}</Typography>
+              <Typography variant="body" color={darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)'}><b>Lanuages:</b> {selectedCountry.languages[0].name}</Typography>
             </div>
           </div>
         </div>
